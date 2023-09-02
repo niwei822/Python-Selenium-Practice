42ff47e563e3d5165e1cb1d93faa7b890475039b
niwei822 <cecily.li.822@gmail.com>
2023-09-02 14:28:50 -0700

    add waits

--- demo3.py
+++ demo3.py
@@ -8,3 +8,35 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+for i in checkboxes:
+    if i.get_attribute("value") == "option2":
+        i.click()
+        i.is_selected()
+        break
+time.sleep(2)
+
+radios = driver.find_elements(By.XPATH, "//input[@type='radio']")
+for radio in radios:
+    if radio.get_attribute("value") == "radio1":
+        radio.click()
+        assert radio.is_selected()
+        break
+time.sleep(2)
+
+assert driver.find_element(By.ID, "displayed-text").is_displayed()
+
+driver.find_element(By.ID, "hide-textbox").click()
+time.sleep(2)
+assert not driver.find_element(By.ID, "displayed-text").is_displayed()
+name = "cici"
+driver.find_element(By.XPATH, "//input[@id='name']").send_keys(name)
+driver.find_element(By.ID, "alertbtn").click()
+
+alert = driver.switch_to.alert
+print(alert.text)
+assert name in alert.text
+#click on ok
+alert.accept()
+#click on cancel
+#alert.dismiss()
---
+++ waits.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.maximize_window()
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+time.sleep(2)
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+assert count > 0
+#Locator Chaining
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
+print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
+
+    
